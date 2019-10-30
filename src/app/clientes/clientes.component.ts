b04cb0d0813371f67de8116695789f7f9e25b2d6
Sebastian Henao <sebastian.henao5741@gmail.com>
2019-10-30 16:18:09 -0500

    Se agrega metodo eliminar
    Se agregan los botones de editar (este rediríge a la pagina de clientes y actualiza la lista) y eliminar

--- src/app/clientes/clientes.component.ts
+++ src/app/clientes/clientes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente';
 import { ClienteService } from './cliente.service';
-
+import swal from 'sweetalert2';
 
 @Component({
   selector: 'app-clientes',
@@ -17,6 +17,32 @@ export class ClientesComponent implements OnInit {
     this.clienteService.getClientes().subscribe(
       clientes => this.clientes = clientes
     );
+  }
+
+  delete(cliente: Cliente): void{
+    swal.fire({
+      title: '¿Está seguro?',
+      text: `¿Seguro que deseas liminar al cliente ${cliente.nombre} ${cliente.apellido}?`,
+      type: 'warning',
+      showCancelButton: true,
+      confirmButtonColor: '#28a745',
+      cancelButtonColor: '#d33',
+      confirmButtonText: 'Si, eliminar!',
+      cancelButtonText: 'No, cancelar'
+    }).then((result) => {
+      if (result.value) {
+
+        this.clienteService.deleteCliente(cliente.id).subscribe(
+          response => {
+            
+            this.clientes = this.clientes.filter(user => user !== cliente)
+            swal.fire(
+              'Cliente eliminado!',
+              'Cliente eliminado con éxito',
+              'success'
+            )}
+        )}
+    })
   }
 
 }
