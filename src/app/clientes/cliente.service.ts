a991d1bd9c3d2b37b08ece0cad52dd6589e1c23d
Sebastian Henao <sebastian.henao5741@gmail.com>
2019-10-30 11:17:05 -0500

    Se agrega consumo al servicio de crear clientes y su navegación al listado de clientes luego de crearlo

--- src/app/clientes/cliente.service.ts
+++ src/app/clientes/cliente.service.ts
@@ -1,18 +1,24 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from './cliente';
 import { Observable, of } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClienteService {
   private ulrEndPoint:string = 'http://localhost:8080/api/clientes';
+
+  private httpHeaders = new HttpHeaders({'Content-Type': 'application/json'})
   constructor(private http: HttpClient) { }
 
   getClientes(): Observable<Cliente[]>{
 
     //return of(CLIENTES);
     return this.http.get<Cliente[]>(this.ulrEndPoint);
+  }
+
+  create(cliente: Cliente): Observable<Cliente> {
+    return this.http.post<Cliente>(this.ulrEndPoint,cliente,{headers: this.httpHeaders});
   }
 }
--- src/app/clientes/form.component.ts
+++ src/app/clientes/form.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Cliente } from './cliente'; 
-import { cleanSession } from 'selenium-webdriver/safari';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-form',
@@ -11,7 +13,7 @@ export class FormComponent implements OnInit {
   private titulo: any = "Crear cliente"
   private cliente: Cliente = new Cliente();
 
-  constructor() { }
+  constructor(private clienteService: ClienteService, private router: Router) { }
 
   ngOnInit() {
   }
@@ -19,6 +21,9 @@ export class FormComponent implements OnInit {
   public create():void{
     console.log("Clicked");
     console.log(this.cliente);
+    this.clienteService.create(this.cliente).subscribe(
+      response => this.router.navigate(['/clientes'])
+    )
   }
 
 }
